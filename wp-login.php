0d53443b9bb9d06b9f3e4a790d6177f7ba3e510e
mdshanewaz <mdshanewazdevp@gmail.com>
2021-01-24 16:53:00 +0600

    wordpress

--- wp-login.php
+++ wp-login.php
@@ -4,18 +4,18 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; My Ecommerce &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/css/dashicons.min.css?ver=5.6' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/css/buttons.min.css?ver=5.6' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/css/forms.min.css?ver=5.6' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/css/l10n.min.css?ver=5.6' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/css/login.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/css/dashicons.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/css/buttons.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/css/forms.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/css/l10n.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/css/login.min.css?ver=5.6' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
-	<link rel="icon" href="/https://mdshanewaz.github.io/WordPress-Proj-1/wp-content/uploads/2021/01/LogoMakr-6xurIF-100x100.png" sizes="32x32" />
-<link rel="icon" href="/https://mdshanewaz.github.io/WordPress-Proj-1/wp-content/uploads/2021/01/LogoMakr-6xurIF-300x300.png" sizes="192x192" />
-<link rel="apple-touch-icon" href="/https://mdshanewaz.github.io/WordPress-Proj-1/wp-content/uploads/2021/01/LogoMakr-6xurIF-300x300.png" />
-<meta name="msapplication-TileImage" content="/wp-content/uploads/2021/01/LogoMakr-6xurIF-300x300.png" />
+	<link rel="icon" href="https://mdshanewaz.github.io/WordPress-Proj-1/wp-content/uploads/2021/01/LogoMakr-6xurIF-100x100.png" sizes="32x32" />
+<link rel="icon" href="https://mdshanewaz.github.io/WordPress-Proj-1/wp-content/uploads/2021/01/LogoMakr-6xurIF-300x300.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://mdshanewaz.github.io/WordPress-Proj-1/wp-content/uploads/2021/01/LogoMakr-6xurIF-300x300.png" />
+<meta name="msapplication-TileImage" content="https://mdshanewaz.github.io/WordPress-Proj-1/wp-content/uploads/2021/01/LogoMakr-6xurIF-300x300.png" />
 	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
 	<script type="text/javascript">
@@ -24,7 +24,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="/https://mdshanewaz.github.io/WordPress-Proj-1/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://mdshanewaz.github.io/WordPress-Proj-1/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -42,34 +42,34 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-									<input type="hidden" name="redirect_to" value="/wp-admin/" />
+									<input type="hidden" name="redirect_to" value="https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/" />
 									<input type="hidden" name="testcookie" value="1" />
 			</p>
 		</form>
 
 					<p id="nav">
-				<a href="/https://mdshanewaz.github.io/WordPress-Proj-1/wp-login.php?action=register">Register</a> | 				<a href="/https://mdshanewaz.github.io/WordPress-Proj-1/my-account/lost-password/">Lost your password?</a>
+				<a href="https://mdshanewaz.github.io/WordPress-Proj-1/wp-login.php?action=register">Register</a> | 				<a href="https://mdshanewaz.github.io/WordPress-Proj-1/my-account/lost-password/">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="/https://mdshanewaz.github.io/WordPress-Proj-1/">
+				<p id="backtoblog"><a href="https://mdshanewaz.github.io/WordPress-Proj-1/">
 		&larr; Go to My Ecommerce		</a></p>
 			</div>
-	<script type='text/javascript' src='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/jquery/jquery.min.js?ver=3.5.1' id='jquery-core-js'></script>
-<script type='text/javascript' src='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
+	<script type='text/javascript' src='https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/jquery/jquery.min.js?ver=3.5.1' id='jquery-core-js'></script>
+<script type='text/javascript' src='https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
 <script type='text/javascript' id='zxcvbn-async-js-extra'>
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/mdshanewaz.github.io\/WordPress-Proj-1\/wp-includes\/js\/zxcvbn.min.js"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
-<script type='text/javascript' src='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
+<script type='text/javascript' src='https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
+<script type='text/javascript' src='https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
 <script type='text/javascript' id='wp-polyfill-js-after'>
-( 'fetch' in window ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"></scr' + 'ipt>' );( document.contains ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"></scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"></scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"></scr' + 'ipt>' );
+( 'fetch' in window ) || document.write( '<script src="https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"></scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"></scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"></scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"></scr' + 'ipt>' );
 </script>
-<script type='text/javascript' src='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/i18n.min.js?ver=326fe7fbfdb407b6edbcfba7e17f3909' id='wp-i18n-js'></script>
+<script type='text/javascript' src='https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/dist/i18n.min.js?ver=326fe7fbfdb407b6edbcfba7e17f3909' id='wp-i18n-js'></script>
 <script type='text/javascript' id='password-strength-meter-js-extra'>
 /* <![CDATA[ */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
@@ -82,14 +82,14 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type='text/javascript' src='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/js/password-strength-meter.min.js?ver=5.6' id='password-strength-meter-js'></script>
-<script type='text/javascript' src='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
+<script type='text/javascript' src='https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/js/password-strength-meter.min.js?ver=5.6' id='password-strength-meter-js'></script>
+<script type='text/javascript' src='https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
 <script type='text/javascript' id='wp-util-js-extra'>
 /* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 /* ]]> */
 </script>
-<script type='text/javascript' src='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/wp-util.min.js?ver=5.6' id='wp-util-js'></script>
+<script type='text/javascript' src='https://mdshanewaz.github.io/WordPress-Proj-1/wp-includes/js/wp-util.min.js?ver=5.6' id='wp-util-js'></script>
 <script type='text/javascript' id='user-profile-js-translations'>
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
@@ -97,7 +97,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type='text/javascript' src='/https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/js/user-profile.min.js?ver=5.6' id='user-profile-js'></script>
+<script type='text/javascript' src='https://mdshanewaz.github.io/WordPress-Proj-1/wp-admin/js/user-profile.min.js?ver=5.6' id='user-profile-js'></script>
 	<div class="clear"></div>
 	</body>
 	</html>
